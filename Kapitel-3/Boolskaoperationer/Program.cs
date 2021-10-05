c21c2306855380c1c0b9ee8867318f083b4293f6
Hugo.Suderbys <hugo.suderbys@lev.ga.ntig.segit config --global user.name>
2021-10-05 11:02:04 +0200

    Tränat på loopar

---
+++ Kapitel-3/Boolskaoperationer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Boolskaoperationer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Åldersgränser i sverige");
+
+
+            system.console.write("Hur gammal är du? ");
+            int ålder = int.Parse(Console.ReadLine());
+
+
+            if (ålder >= 15)
+            {
+              Console.WriteLine("Du får köra moped!");
+            }
+
+            if (ålder >= 18)
+            {
+                Console.WriteLine("Du får köra bil");
+            }
+        }
+    }
+}
---
+++ Kapitel-3/Gissa ett Tal/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Kapitel_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Spel - gissa ett tal mellan 1 och 100.");
+
+            while (true)
+            {
+                            Random tärning = new Random();
+            int slumptal = tärning.Next(1, 101);
+
+            int räknare = 0;
+
+            while (true)
+            {
+                räknare++;
+
+
+                             Console.Write("Gissa ett tal (1-100)");
+            int gissning = int.Parse(Console.ReadLine());
+
+            if (gissning == slumptal)
+            {
+                Console.WriteLine($"Bra gissat! Du gjorde det på {räknare} försök");
+                break;
+            }
+
+            if (gissning < slumptal)
+            {
+                Console.WriteLine("För lågt!");
+            }
+            else
+            {
+                Console.WriteLine("För högt!");
+            }
+            }
+            }
+        }
+    }
+}
