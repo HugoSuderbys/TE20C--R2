76fbe67ef68248f6d296a164398a5d5be10ca108
Hugo.Suderbys <hugo.suderbys@lev.ga.ntig.segit config --global user.name>
2021-10-12 12:16:12 +0200

    Uppgift 4.1

---
+++ Kapitel-3/Uppgift karim borta/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift_karim_borta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("I vilket land är Angela Merkel president?");
+            string svar = Console.ReadLine();
+            while (svar != "tyskland")
+            {
+                 Console.WriteLine("Du svarade fel, försök igen");
+                 svar = Console.ReadLine();
+                 
+            }
+            Console.WriteLine("Grattis! Du svarade rätt!");
+        }
+    }
+}
