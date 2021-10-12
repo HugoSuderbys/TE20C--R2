e4d3e251eadd8c9a10430280f506507179b86454
Hugo.Suderbys <hugo.suderbys@lev.ga.ntig.segit config --global user.name>
2021-10-12 13:12:19 +0200

    Uppgift 4.2

---
+++ Kapitel 4/Uppgift 4.3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Uppgift_4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Detta är ett omvandlingsprogram");
+
+            string val = "";
+
+            while (val != "3")
+
+            {
+                Console.WriteLine();
+                Console.WriteLine("Välj ett av följande alternativ");
+                Console.WriteLine("1. Omvandla meter till kilometer");
+                Console.WriteLine("2. Omvandla kilometer till meter");
+                Console.WriteLine("3. Avsluta programet");
+                val = Console.ReadLine();
+
+                switch (val)
+                {
+                    case "1":
+                    Console.WriteLine("Skriv in längd i meter");
+                    double antalmeter = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"längden du skrev in är detsamma som {antalmeter / 1000} km");
+                        break;
+
+                    case "2":
+                    Console.WriteLine("Skriv in en längd i km");
+                    double antalKm = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"Längden du skrev in är detsamma som {antalKm * 1000} meter");
+                        break;
+                        
+                    case "3":
+                    Console.WriteLine("Programmet avslutas...");
+                        break;
+
+                    default:
+                    break;
+                }
+            }
+            
+
+
+        }
+    }
+}
